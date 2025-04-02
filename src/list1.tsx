d169620b4d81f4d6107cdba4e37cfc0125074db6
vinam.nguyen <vinamnguyen500@gmail.com>
2025-04-01 23:36:16 -0400

    edited app.tsx, hobby1.tsx, intro.tsx, and title.tsx

---
+++ src/list1.tsx
@@ -0,0 +1,16 @@
+const List1 = () => {
+  return (
+      <><
+          h2>My Top 5 Greatest NBA Players of All Time</h2>
+          <ol>
+              <li>LeBron James</li>
+              <li>Michael Jordan</li>
+              <li>Kareem Abdul Jabbar</li>
+              <li>Magic Johnson</li>
+              <li>Larry Bird</li>
+          </ol>
+      </>
+  )
+}
+
+export default List1;
---
+++ src/table1.tsx
@@ -0,0 +1,51 @@
+const Table1 = () => {
+  return (
+      <table>
+          <caption><h2>My Top 5 Greatest NBA Players of All Time By Position</h2></caption>
+          <tr>
+              <th>Point Guard</th>
+              <th>Shooting Guard</th>
+              <th>Small Forward</th>
+              <th>Power Forward</th>
+              <th>Center</th>
+          </tr>
+          <tr>
+              <td>Magic Johnson</td>
+              <td>Michael Jordan</td>
+              <td>LeBron James</td>
+              <td>Tim Duncan</td>
+              <td>Kareem Abdul Jabbar</td>
+          </tr>
+          <tr>
+              <td>Stephen Curry</td>
+              <td>Kobe Bryant</td>
+              <td>Larry Bird</td>
+              <td>Karl Malone</td>
+              <td>Hakeem Olajuwon</td>
+          </tr>
+          <tr>
+              <td>Oscar Robertson</td>
+              <td>Dwyane Wade</td>
+              <td>Kevin Durant</td>
+              <td>Kevin Garnett</td>
+              <td>Shaquille O'Neal</td>
+          </tr>
+          <tr>
+              <td>Isiah Thomas</td>
+              <td>James Harden</td>
+              <td>Julius Erving</td>
+              <td>Dirk Nowitzki</td>
+              <td>Bill Russell</td>
+          </tr>
+          <tr>
+              <td>Jerry West</td>
+              <td>Allen Iverson</td>
+              <td>Elgin Baylor</td>
+              <td>Charles Barkley</td>
+              <td>Wilt Chamberlain</td>
+          </tr>
+      </table>
+  )
+}
+
+export default Table1;
